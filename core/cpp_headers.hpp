206416ac3dfbc1766f367e92fa67689178c3b558
Florian Ziesche <florian.ziesche@gmail.com>
2013-11-07 19:16:03 +0100

    * spaces -> tabs

--- core/cpp_headers.hpp
+++ core/cpp_headers.hpp
@@ -104,28 +104,28 @@ template <class T, size_t N> const T* cend(const T (&array)[N]) { return array +
 // code from: http://isocpp.org/files/papers/N3656.txt
 
 template<class T> struct floor_Unique_if {
-    typedef unique_ptr<T> _Single_object;
+	typedef unique_ptr<T> _Single_object;
 };
 
 template<class T> struct floor_Unique_if<T[]> {
-    typedef unique_ptr<T[]> _Unknown_bound;
+	typedef unique_ptr<T[]> _Unknown_bound;
 };
 
 template<class T, size_t N> struct floor_Unique_if<T[N]> {
-    typedef void _Known_bound;
+	typedef void _Known_bound;
 };
 
 template<class T, class... Args>
 typename floor_Unique_if<T>::_Single_object
 make_unique(Args&&... args) {
-    return unique_ptr<T>(new T(std::forward<Args>(args)...));
+	return unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 template<class T>
 typename floor_Unique_if<T>::_Unknown_bound
 make_unique(size_t n) {
-    typedef typename remove_extent<T>::type U;
-    return unique_ptr<T>(new U[n]());
+	typedef typename remove_extent<T>::type U;
+	return unique_ptr<T>(new U[n]());
 }
 
 template<class T, class... Args>
