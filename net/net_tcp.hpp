d8ddb07e8591bfb73a6ad6a7b9539bea8f0c4629
Florian Ziesche <florian.ziesche@gmail.com>
2013-11-16 09:36:41 +0100

    * now requires TLS 1.2 and OpenSSL 1.0.1+ for any encrypted TCP connection  * restructured the xcode project, so that /Library/Frameworks doesn't have to be symlinked into /Applications/Xcode.app/... any more  * now requires homebrew (http://brew.sh) on os x and a manually compiled/installed openssl lib (because os x only provides openssl 0.9.8)

--- net/net_tcp.hpp
+++ net/net_tcp.hpp
@@ -58,7 +58,7 @@ namespace floor_net {
 		boost::asio::ssl::stream<tcp::socket> socket;
 		tcp::socket& socket_layer; // ref to the actual socket layer
 		protocol_details<true>(boost::asio::io_service& io_service) :
-		context(io_service, boost::asio::ssl::context::tlsv1), socket(io_service, context), socket_layer(socket.next_layer()) {
+		context(io_service, boost::asio::ssl::context::tlsv12), socket(io_service, context), socket_layer(socket.next_layer()) {
 			context.set_default_verify_paths();
 			socket.set_verify_mode(boost::asio::ssl::verify_peer);
 			socket.set_verify_callback(boost::bind(&protocol_details<true>::verify_certificate, this, _1, _2));
