ca9b95b34a9c914a8a14f0f920697b8e3a72c9e0
Florian Ziesche <florian.ziesche@gmail.com>
2014-12-31 13:11:46 +0100

    * update copyright year to 2015, and with that: happy new year :)

--- compute/opencl/opencl_buffer.hpp
+++ compute/opencl/opencl_buffer.hpp
@@ -1,6 +1,6 @@
 /*
  *  Flo's Open libRary (floor)
- *  Copyright (C) 2004 - 2014 Florian Ziesche
+ *  Copyright (C) 2004 - 2015 Florian Ziesche
  *  
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
