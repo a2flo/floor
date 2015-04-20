115c6197dc792eeea88862e2ccec4cd9d0cbca84
Florian Ziesche <florian.ziesche@gmail.com>
2015-04-20 19:44:21 +0200

    * added simple pkg.sh script to package up the compute clang/llvm binaries/files and patched libcxx include folder

---
+++ etc/llvm35/pkg.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VERSION=$(date +%Y%m%d_%H%M)
+mkdir compute_clang_${VERSION}
+cd compute_clang_${VERSION}
+cp ../build/Release/bin/clang++ compute_clang
+cp ../build/Release/bin/llc compute_llc
+cp ../build/Release/bin/llvm-dis compute_dis
+cp ../build/Release/bin/llvm-as compute_as
+cp ../build/Release/bin/spir-encoder .
+
+mkdir libcxx
+cp -R ../libcxx/clang libcxx/
+cp -R ../libcxx/include libcxx/
+cp ../libcxx/{LICENSE.TXT,CREDITS.TXT} libcxx/
+
+cd ..
+
+zip -r compute_clang_${VERSION}.zip compute_clang_${VERSION}
