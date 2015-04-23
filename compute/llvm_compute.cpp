b3f81492834be5a9a17a9c9c9215ea4ad5b22ec0
Florian Ziesche <florian.ziesche@gmail.com>
2015-04-23 21:35:28 +0200

    * added visual studio project files (needs http://llvm.org/builds of course), this has include+lib paths set to the current intel, amd and nvidia sdks (either intel or amd sdk is required, and cuda sdk is required), floor include/libs/deps paths are also set, but probably need to modified depending on the system setup

--- compute/llvm_compute.cpp
+++ compute/llvm_compute.cpp
@@ -357,10 +357,8 @@ pair<string, vector<llvm_compute::kernel_info>> llvm_compute::compile_input(cons
 	}
 	else if(target == TARGET::AIR) {
 		// this exchanges the module header/target to the one apple expects
-		//const regex rx_module_id("; ModuleID = '(.*)'");
 		static const regex rx_datalayout("target datalayout = \"(.*)\"");
 		static const regex rx_triple("target triple = \"(.*)\"");
-		//ir_code = regex_replace(ir_code, rx_module_id, "; ModuleID = '" + expected_filename + "'");
 		ir_output = regex_replace(ir_output, rx_datalayout,
 								  "target datalayout \"e-i64:64-f80:128-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-n8:16:32\"");
 		ir_output = regex_replace(ir_output, rx_triple, "target triple = \"air64-apple-ios8.1.0\"");
