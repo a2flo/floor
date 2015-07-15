0c51637b37a74a151c7b03de940bf8b935b45b54
Florian Ziesche <florian.ziesche@gmail.com>
2015-07-15 15:41:20 +0200

    * more host image progress: should be able to write all supported normalized formats now; moved validation checks to separate functions, because they're called by both read and write functions; combined non-normalized int/uint reads + added 64-bit format support

--- compute/device/host_image.hpp
+++ compute/device/host_image.hpp
@@ -72,9 +72,8 @@ template <COMPUTE_IMAGE_TYPE image_type> static constexpr auto compute_image_bpc
 	}};
 }
 
-template <COMPUTE_IMAGE_TYPE image_type, size_t N,
-		  enable_if_t<(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) != COMPUTE_IMAGE_TYPE::FLOAT>* = nullptr>
-floor_inline_always auto extract_channels(const uint8_t (&raw_data)[N]) {
+template <COMPUTE_IMAGE_TYPE image_type>
+static constexpr void normalized_format_validity_check() {
 	constexpr const auto image_format = (image_type & COMPUTE_IMAGE_TYPE::__FORMAT_MASK);
 	
 	// validity checking: depth reading is handled elsewhere
@@ -93,13 +92,43 @@ floor_inline_always auto extract_channels(const uint8_t (&raw_data)[N]) {
 				  image_format != COMPUTE_IMAGE_TYPE::FORMAT_11_11_10 &&
 				  image_format != COMPUTE_IMAGE_TYPE::FORMAT_12_12_12 &&
 				  image_format != COMPUTE_IMAGE_TYPE::FORMAT_12_12_12_12, "unsupported format!");
+}
+
+template <COMPUTE_IMAGE_TYPE image_type>
+static constexpr void depth_format_validity_check() {
+	constexpr const auto data_type = (image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK);
+	static_assert(data_type == COMPUTE_IMAGE_TYPE::FLOAT ||
+				  data_type == COMPUTE_IMAGE_TYPE::UINT, "invalid depth data type!");
+	
+	constexpr const auto image_format = (image_type & COMPUTE_IMAGE_TYPE::__FORMAT_MASK);
+	constexpr const bool has_stencil = has_flag<COMPUTE_IMAGE_TYPE::FLAG_STENCIL>(image_type);
+	static_assert((!has_stencil &&
+				   ((image_format == COMPUTE_IMAGE_TYPE::FORMAT_16 && data_type == COMPUTE_IMAGE_TYPE::UINT) ||
+					(image_format == COMPUTE_IMAGE_TYPE::FORMAT_24 && data_type == COMPUTE_IMAGE_TYPE::UINT) ||
+					image_format == COMPUTE_IMAGE_TYPE::FORMAT_32 /* supported by both */)) ||
+				  (has_stencil &&
+				   ((image_format == COMPUTE_IMAGE_TYPE::FORMAT_24_8 && data_type == COMPUTE_IMAGE_TYPE::UINT) ||
+					(image_format == COMPUTE_IMAGE_TYPE::FORMAT_32_8 && data_type == COMPUTE_IMAGE_TYPE::FLOAT))),
+				  "invalid depth format!");
+	
+	constexpr const auto channel_count = image_channel_count(image_type);
+	static_assert((!has_stencil && channel_count == 1) ||
+				  (has_stencil && channel_count == 2), "invalid channel count for depth format!");
+}
+
+template <COMPUTE_IMAGE_TYPE image_type, size_t N,
+		  enable_if_t<(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) != COMPUTE_IMAGE_TYPE::FLOAT>* = nullptr>
+floor_inline_always auto extract_channels(const uint8_t (&raw_data)[N]) {
+	constexpr const auto image_format = (image_type & COMPUTE_IMAGE_TYPE::__FORMAT_MASK);
+	normalized_format_validity_check<image_type>();
 	
 	// get bits of each channel (0 if channel doesn't exist)
 	constexpr const auto bpc = compute_image_bpc<image_type>();
+	constexpr const auto max_bpc = max(max(bpc[0], bpc[1]), max(bpc[2], bpc[3]));
 	// figure out which type we need to store each channel (max size is used)
-	typedef typename image_sized_data_type<image_type, max(max(bpc[0], bpc[1]), max(bpc[2], bpc[3]))>::type channel_type;
+	typedef typename image_sized_data_type<image_type, max_bpc>::type channel_type;
 	// and the appropriate unsigned format
-	typedef typename image_sized_data_type<COMPUTE_IMAGE_TYPE::UINT, max(max(bpc[0], bpc[1]), max(bpc[2], bpc[3]))>::type uchannel_type;
+	typedef typename image_sized_data_type<COMPUTE_IMAGE_TYPE::UINT, max_bpc>::type uchannel_type;
 	
 	// and now for bit voodoo:
 	constexpr const uint32_t channel_count = image_channel_count(image_type);
@@ -122,18 +151,8 @@ floor_inline_always auto extract_channels(const uint8_t (&raw_data)[N]) {
 		case COMPUTE_IMAGE_TYPE::FORMAT_64:
 			memcpy(&ret[0], raw_data, N);
 			break;
-			
-		// special formats (TODO: implement this when needed, opencl/cuda don't support this either)
-		case COMPUTE_IMAGE_TYPE::FORMAT_3_3_2:
-		case COMPUTE_IMAGE_TYPE::FORMAT_5_5_5:
-		case COMPUTE_IMAGE_TYPE::FORMAT_5_5_5_1:
-		case COMPUTE_IMAGE_TYPE::FORMAT_5_6_5:
-		case COMPUTE_IMAGE_TYPE::FORMAT_9_9_9_5:
-		case COMPUTE_IMAGE_TYPE::FORMAT_10:
-		case COMPUTE_IMAGE_TYPE::FORMAT_10_10_10_2:
-		case COMPUTE_IMAGE_TYPE::FORMAT_11_11_10:
-		case COMPUTE_IMAGE_TYPE::FORMAT_12_12_12:
-		case COMPUTE_IMAGE_TYPE::FORMAT_12_12_12_12:
+		
+		//  TODO: special formats, implement this when needed, opencl/cuda don't support this either
 		default:
 			floor_unreachable();
 	}
@@ -160,6 +179,87 @@ floor_inline_always auto extract_channels(const uint8_t (&raw_data)[N]) {
 template <COMPUTE_IMAGE_TYPE image_type, size_t N,
 		  enable_if_t<(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::FLOAT>* = nullptr>
 floor_inline_always auto extract_channels(const uint8_t (&raw_data)[N]) {
+	// this is a dummy function and never called, but necessary for compilation
+	return vector_n<uint8_t, image_channel_count(image_type)> {};
+}
+
+
+template <COMPUTE_IMAGE_TYPE image_type,
+		  enable_if_t<(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) != COMPUTE_IMAGE_TYPE::FLOAT>* = nullptr>
+floor_inline_always auto insert_channels(const vector_n<float, image_channel_count(image_type)>& color) {
+	constexpr const auto image_format = (image_type & COMPUTE_IMAGE_TYPE::__FORMAT_MASK);
+	constexpr const auto data_type = (image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK);
+	constexpr const uint32_t channel_count = image_channel_count(image_type);
+	normalized_format_validity_check<image_type>();
+	
+	// figure out how much data (raw data) must be returned/created
+	constexpr const auto bpp = image_bytes_per_pixel(image_type);
+	array<uint8_t, bpp> raw_data;
+	
+	// scale fp vals to the expected int/uint size
+	constexpr const auto bpc = compute_image_bpc<image_type>();
+	constexpr const auto max_bpc = max(max(bpc[0], bpc[1]), max(bpc[2], bpc[3]));
+	typedef typename image_sized_data_type<image_type, max_bpc>::type channel_type;
+	typedef typename image_sized_data_type<COMPUTE_IMAGE_TYPE::UINT, max_bpc>::type uchannel_type;
+	typedef conditional_t<max_bpc <= 8u, float, conditional_t<max_bpc <= 16u, double, long double>> fp_scale_type;
+	vector_n<channel_type, channel_count> scaled_color;
+#pragma clang loop unroll(full) vectorize(enable) interleave(enable)
+	for(uint32_t i = 0; i < channel_count; ++i) {
+		// scale with 2^(bpc (- 1 if signed)) - 1 (e.g. 255 for unsigned 8-bit, 127 for signed 8-bit)
+		scaled_color[i] = channel_type(color[i] * fp_scale_type((uchannel_type(1)
+																 << uchannel_type(max(bpc[i], 1u)
+																				  - (data_type == COMPUTE_IMAGE_TYPE::UINT ? 0u : 1u)))
+																- uchannel_type(1)));
+	}
+	
+	constexpr const bool is_signed_format = (data_type == COMPUTE_IMAGE_TYPE::INT);
+	switch(image_format) {
+		// uniform formats
+		case COMPUTE_IMAGE_TYPE::FORMAT_2:
+			memset(&raw_data[0], 0, bpp);
+			if(!is_signed_format) {
+				for(uint32_t i = 0; i < channel_count; ++i) {
+					raw_data[0] |= (scaled_color[i] & 0b11u) << (6u - 2u * i);
+				}
+			}
+			else {
+				// since the sign bit isn't the msb, it must be handled manually
+				for(uint32_t i = 0; i < channel_count; ++i) {
+					raw_data[0] |= ((scaled_color[i] & 0b1u) | (scaled_color[i] < 0 ? 0b10u : 0u)) << (6u - 2u * i);
+				}
+			}
+			break;
+		case COMPUTE_IMAGE_TYPE::FORMAT_4:
+			memset(&raw_data[0], 0, bpp);
+			if(!is_signed_format) {
+				for(uint32_t i = 0; i < channel_count; ++i) {
+					raw_data[i / 2u] |= (scaled_color[i] & 0b1111u) << (i % 2u == 0 ? 4u : 0u);
+				}
+			}
+			else {
+				// since the sign bit isn't the msb, it must be handled manually
+				for(uint32_t i = 0; i < channel_count; ++i) {
+					raw_data[i / 2u] |= ((scaled_color[i] & 0b111u) | (scaled_color[i] < 0 ? 0b1000u : 0u)) << (i % 2u == 0 ? 4u : 0u);
+				}
+			}
+			break;
+		case COMPUTE_IMAGE_TYPE::FORMAT_8:
+		case COMPUTE_IMAGE_TYPE::FORMAT_16:
+		case COMPUTE_IMAGE_TYPE::FORMAT_32:
+		case COMPUTE_IMAGE_TYPE::FORMAT_64:
+			memcpy(&raw_data[0], &scaled_color, bpp);
+			break;
+			
+		//  TODO: special formats, implement this when needed, opencl/cuda don't support this either
+		default:
+			floor_unreachable();
+	}
+	
+	return raw_data;
+}
+template <COMPUTE_IMAGE_TYPE image_type,
+		  enable_if_t<(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::FLOAT>* = nullptr>
+floor_inline_always auto insert_channels(const vector_n<float, image_channel_count(image_type)>&) {
 	// this is a dummy function and never called, but necessary for compilation
 	return vector_n<uint8_t, image_channel_count(image_type)> {};
 }
@@ -226,25 +326,11 @@ template <COMPUTE_IMAGE_TYPE image_type, bool readable, bool writable,
 floor_inline_always auto read(const host_device_image<image_type, readable, writable>* img, int2 coord) {
 	static_assert(readable, "image must be readable!");
 	
-	// validate all the things
-	constexpr const auto data_type = (image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK);
-	static_assert(data_type == COMPUTE_IMAGE_TYPE::FLOAT ||
-				  data_type == COMPUTE_IMAGE_TYPE::UINT, "invalid depth data type!");
-	
 	constexpr const auto image_format = (image_type & COMPUTE_IMAGE_TYPE::__FORMAT_MASK);
 	constexpr const bool has_stencil = has_flag<COMPUTE_IMAGE_TYPE::FLAG_STENCIL>(image_type);
-	static_assert((!has_stencil &&
-				   ((image_format == COMPUTE_IMAGE_TYPE::FORMAT_16 && data_type == COMPUTE_IMAGE_TYPE::UINT) ||
-					(image_format == COMPUTE_IMAGE_TYPE::FORMAT_24 && data_type == COMPUTE_IMAGE_TYPE::UINT) ||
-					image_format == COMPUTE_IMAGE_TYPE::FORMAT_32 /* supported by both */)) ||
-				  (has_stencil &&
-				   ((image_format == COMPUTE_IMAGE_TYPE::FORMAT_24_8 && data_type == COMPUTE_IMAGE_TYPE::UINT) ||
-					(image_format == COMPUTE_IMAGE_TYPE::FORMAT_32_8 && data_type == COMPUTE_IMAGE_TYPE::FLOAT))),
-				  "invalid depth format!");
 	
-	constexpr const auto channel_count = image_channel_count(image_type);
-	static_assert((!has_stencil && channel_count == 1) ||
-				  (has_stencil && channel_count == 2), "invalid channel count for depth format!");
+	// validate all the things
+	depth_format_validity_check<image_type>();
 	
 	// depth is always returned as a float, with stencil a a <float, 8-bit uint> pair
 	// NOTE: neither opencl, nor cuda support reading depth+stencil images, so a proper return type is unclear right now
@@ -298,7 +384,8 @@ floor_inline_always auto read(const host_device_image<image_type, readable, writ
 
 template <COMPUTE_IMAGE_TYPE image_type, bool readable, bool writable,
 		  enable_if_t<(!has_flag<COMPUTE_IMAGE_TYPE::FLAG_NORMALIZED>(image_type) &&
-					   (image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::INT)>* = nullptr>
+					   ((image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::INT ||
+						(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::UINT))>* = nullptr>
 floor_inline_always auto read(const host_device_image<image_type, readable, writable>* img, int2 coord) {
 	static_assert(readable, "image must be readable!");
 	
@@ -307,47 +394,72 @@ floor_inline_always auto read(const host_device_image<image_type, readable, writ
 	uint8_t raw_data[bpp];
 	memcpy(raw_data, &img->data[img->coord_to_offset(coord)], bpp);
 	
+	// for compatibility with opencl/cuda, always return 32-bit values for anything <= 32-bit (and 64-bit values for > 32-bit)
+	constexpr const bool is_signed_format = ((image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::INT);
+	typedef conditional_t<image_bits_of_channel(image_type, 0) <= 32,
+						  conditional_t<is_signed_format, int32_t, uint32_t>,
+						  conditional_t<is_signed_format, int64_t, uint64_t>> ret_type;
 	constexpr const auto channel_count = image_channel_count(image_type);
-	vector_n<int32_t, channel_count> ret =
+	vector_n<ret_type, channel_count> ret =
 		*(vector_n<typename image_sized_data_type<image_type, image_bits_of_channel(image_type, 0)>::type, channel_count>*)raw_data;
-	return host_image_fit_return_type<int32_t, channel_count>(ret);
-}
-
-template <COMPUTE_IMAGE_TYPE image_type, bool readable, bool writable,
-		  enable_if_t<(!has_flag<COMPUTE_IMAGE_TYPE::FLAG_NORMALIZED>(image_type) &&
-					   (image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::UINT)>* = nullptr>
-floor_inline_always auto read(const host_device_image<image_type, readable, writable>* img, int2 coord) {
-	static_assert(readable, "image must be readable!");
-	
-	// read/copy raw data
-	constexpr const size_t bpp = image_bytes_per_pixel(image_type);
-	uint8_t raw_data[bpp];
-	memcpy(raw_data, &img->data[img->coord_to_offset(coord)], bpp);
-	
-	constexpr const auto channel_count = image_channel_count(image_type);
-	vector_n<uint32_t, channel_count> ret =
-		*(vector_n<typename image_sized_data_type<image_type, image_bits_of_channel(image_type, 0)>::type, channel_count>*)raw_data;
-	return host_image_fit_return_type<uint32_t, channel_count>(ret);
+	return host_image_fit_return_type<ret_type, channel_count>(ret);
 }
 
 // image write functions
-template <COMPUTE_IMAGE_TYPE image_type, bool readable, bool writable>
-void write(host_device_image<image_type, readable, writable>* img, const int2& coord, const float4& color) {
+template <COMPUTE_IMAGE_TYPE image_type, bool readable, bool writable,
+		  typename color_type = conditional_t<image_channel_count(image_type) == 1, float, vector_n<float, image_channel_count(image_type)>>,
+		  enable_if_t<((has_flag<COMPUTE_IMAGE_TYPE::FLAG_NORMALIZED>(image_type) ||
+						(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::FLOAT) &&
+					   !has_flag<COMPUTE_IMAGE_TYPE::FLAG_DEPTH>(image_type))>* = nullptr>
+void write(host_device_image<image_type, readable, writable>* img, const int2& coord, const color_type& color) {
 	static_assert(writable, "image must be writable!");
 	
+	constexpr const auto data_type = (image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK);
 	constexpr const auto channel_count = image_channel_count(image_type);
 	const auto offset = img->coord_to_offset(coord);
-	// TODO: same as read, but in reverse!
 	
-	// for testing purposes:
-	if((image_type & COMPUTE_IMAGE_TYPE::__FORMAT_MASK) == COMPUTE_IMAGE_TYPE::FORMAT_8 &&
-	   (image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::UINT &&
-	   has_flag<COMPUTE_IMAGE_TYPE::FLAG_NORMALIZED>(image_type)) {
-#pragma clang loop unroll(full) vectorize(enable) interleave(enable)
-		for(uint32_t i = 0; i < channel_count; ++i) {
-			img->data[offset + i] = const_math::clamp((uint32_t)(color[i] * 255.0f), 0u, 255u);
-		}
+	if(data_type == COMPUTE_IMAGE_TYPE::FLOAT) {
+		// for float formats, just pass-through
+		memcpy(&img->data[offset], &color, sizeof(color_type));
 	}
+	else {
+		// for integer formats, need to rescale to storage integer format and convert
+		const auto converted_color = insert_channels<image_type>(color);
+		
+		// note that two pixels never share the same byte -> can just memcpy, even for crooked formats
+		memcpy(&img->data[offset], &converted_color, sizeof(decltype(converted_color)));
+	}
+}
+
+
+template <COMPUTE_IMAGE_TYPE image_type, bool readable, bool writable,
+		  /* float depth value, or float depth + uint8_t stencil */
+		  typename color_type = conditional_t<image_channel_count(image_type) == 1, float, pair<float, uint8_t>>,
+		  enable_if_t<has_flag<COMPUTE_IMAGE_TYPE::FLAG_DEPTH>(image_type)>* = nullptr>
+void write(host_device_image<image_type, readable, writable>* img, const int2& coord, const color_type& color) {
+	static_assert(writable, "image must be writable!");
+	depth_format_validity_check<image_type>();
+	
+	const auto offset = img->coord_to_offset(coord);
+	
+	// TODO: !
+}
+
+template <COMPUTE_IMAGE_TYPE image_type, bool readable, bool writable,
+		  typename scalar_type = conditional_t<image_bits_of_channel(image_type, 0) <= 32,
+											   conditional_t<(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::INT, int32_t, uint32_t>,
+											   conditional_t<(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::INT, int64_t, uint64_t>>,
+		  typename color_type = conditional_t<image_channel_count(image_type) == 1,
+											  scalar_type,
+											  vector_n<scalar_type, image_channel_count(image_type)>>,
+		  enable_if_t<(!has_flag<COMPUTE_IMAGE_TYPE::FLAG_NORMALIZED>(image_type) &&
+					   ((image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::INT ||
+						(image_type & COMPUTE_IMAGE_TYPE::__DATA_TYPE_MASK) == COMPUTE_IMAGE_TYPE::UINT))>* = nullptr>
+void write(host_device_image<image_type, readable, writable>* img, const int2& coord, const color_type& color) {
+	static_assert(writable, "image must be writable!");
+	const auto offset = img->coord_to_offset(coord);
+	
+	// TODO: !
 }
 
 #endif
