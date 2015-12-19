38ef93985adcc96256aee07fc397da3519fb5b7f
Florian Ziesche <florian.ziesche@gmail.com>
2015-12-19 14:51:01 +0100

    * updated timer classes: floor_timer_t/floor_timer is now the single use timer (renamed from floor_timer2), floor_multi_timer_t/floor_multi_timer is now the incremental multi-use timer (+updated to use chrono functionality; renamed from floor_timer)

--- core/timer.hpp
+++ core/timer.hpp
@@ -21,56 +21,12 @@
 
 #include <floor/core/platform.hpp>
 
-class floor_timer {
-public:
-	floor_timer() { add("start", false); }
-	~floor_timer() {}
-	
-	void add(const string& name, const bool print_diff = true) {
-		entries.emplace_back(name, SDL_GetPerformanceCounter());
-		if(print_diff) {
-			print_entry(entries.end() - 1);
-		}
-	}
-	
-	void end(const bool print_all = false) const {
-		if(print_all) {
-			for(auto entry = entries.cbegin(); entry != entries.cend(); entry++) {
-				print_entry(entry);
-			}
-		}
-		const auto total_diff = entries.back().second - entries.front().second;
-		log_undecorated("[TOTAL] %_s # %_ms # %_",
-						double(total_diff) / double(ticks_per_second),
-						double(total_diff) / double(ticks_per_second / 1000ull),
-						total_diff);
-	}
-	
-protected:
-	const unsigned long long int ticks_per_second { SDL_GetPerformanceFrequency() };
-	vector<pair<const string, const unsigned long long int>> entries;
-	
-	void print_entry(const decltype(entries)::const_iterator& iter) const {
-		// don't print the start entry
-		if(iter == entries.cbegin()) return;
-		
-		const auto prev_iter = iter - 1;
-		const auto diff = iter->second - prev_iter->second;
-		log_undecorated("[%_] %_ms # %_",
-						iter->first,
-						double(diff) / double(ticks_per_second / 1000ull),
-						diff);
-	}
-	
-};
-
-// TODO: merge with other timer class
-// timer needs a monotonic/steady clock, try using high_resolution_clock as default if it is steady, otherwise use steady_clock
+//! simple timer class based on std chrono functionality and capable of using arbitrary duration types (defaults to ms)
+//! NOTE: timer needs a monotonic/steady clock, uses high_resolution_clock as a default if it is steady, otherwise uses steady_clock
 template <class clock_type = typename conditional<chrono::high_resolution_clock::is_steady,
 												  chrono::high_resolution_clock,
 												  chrono::steady_clock>::type>
-//! simple timer class based on std chrono functionality and capable of using arbitrary duration types (defaults to ms)
-class floor_timer2_t {
+class floor_timer_t {
 public:
 	//! "starts" the timer: returns the current time_point of a monotonic clock
 	static chrono::time_point<clock_type> start() {
@@ -86,12 +42,65 @@ public:
 	
 protected:
 	// static class
-	floor_timer2_t(const floor_timer2_t&) = delete;
-	~floor_timer2_t() = delete;
-	floor_timer2_t& operator=(const floor_timer2_t&) = delete;
+	floor_timer_t(const floor_timer_t&) = delete;
+	~floor_timer_t() = delete;
+	floor_timer_t& operator=(const floor_timer_t&) = delete;
 	
 };
 //! floor_timer_t with default clock_type (this is for easier use, so <> doesn't have to be used all the time)
-using floor_timer2 = floor_timer2_t<>;
+using floor_timer = floor_timer_t<>;
+
+//! multi-timer, can add multiple incrementally timed entries, each entry with its own name, will compute total at the end
+template <class clock_type = typename conditional<chrono::high_resolution_clock::is_steady,
+												  chrono::high_resolution_clock,
+												  chrono::steady_clock>::type>
+class floor_multi_timer_t {
+public:
+	floor_multi_timer_t() { add("start", false); }
+	
+	void add(const string& name, const bool print_diff = true) {
+		entries.emplace_back(name, clock_type::now());
+		if(print_diff) {
+			print_entry(prev(entries.cend()));
+		}
+	}
+	
+	void end(const bool print_all = false) const {
+		if(print_all) {
+			for(auto entry = entries.cbegin(); entry != entries.cend(); ++entry) {
+				print_entry(entry);
+			}
+		}
+		const auto total_diff = entries.back().second - entries.front().second;
+		log_undecorated("[TOTAL] %_s # %_ms # %_",
+						to_s(entries.back().second, entries.front().second),
+						to_ms(entries.back().second, entries.front().second),
+						(uint64_t)chrono::duration_cast<chrono::nanoseconds>(entries.back().second, entries.front().second).count());
+	}
+	
+protected:
+	typedef chrono::time_point<clock_type> time_entry;
+	vector<pair<const string, time_entry>> entries;
+	
+	void print_entry(const typename decltype(entries)::const_iterator& iter) const {
+		// don't print the start entry
+		if(iter == entries.cbegin()) return;
+		
+		log_undecorated("[%_] %_ms # %_",
+						iter->first,
+						to_ms(iter->second, prev(iter)->second),
+						to_s(iter->second, prev(iter)->second));
+	}
+	
+	static double to_ms(const time_entry& first, const time_entry& second) {
+		return double(chrono::duration_cast<chrono::microseconds>(first - second).count()) / 1000.0;
+	}
+	
+	static double to_s(const time_entry& first, const time_entry& second) {
+		return double(chrono::duration_cast<chrono::milliseconds>(first - second).count()) / 1000.0;
+	}
+	
+};
+using floor_multi_timer = floor_multi_timer_t<>;
 
 #endif
