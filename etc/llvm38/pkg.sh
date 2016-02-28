06ea2e462abbcbab39b4996b117d33a59a67690b
Florian Ziesche <florian.ziesche@gmail.com>
2016-02-28 18:21:55 +0100

    * added *very* preliminary clang/llvm 3.8.0 toolchain patches:    * this includes (almost) all toolchain changes from 3.5.2, ported to 3.8.0 (will just work with ptx for now, as there are still some issues with aggregate expansion and no 3.8 -> 3.5 bitcode/ir converter exists yet, which is necessary to support metal and spir 1.2)    * this includes SPIRV-LLVM from khronos, so that SPIR-V can be generated *soon* (https://github.com/KhronosGroup/SPIRV-LLVM branch: khronos/spirv-3.6.1 commit: 90e0274a8e9c76a7aed8f49012aa852cf87b4d16)    * this includes SPIR from khronos, so that SPIR can be converted to SPIR-V + other opencl/spir things (https://github.com/KhronosGroup/SPIR branch: spirv-1.0 commit: ab4997313eb25f0aa164984623c39f94543bf277)    * NOTE: all non-essential parts of SPIRV-LLVM and SPIR from khronos are not included in this patch to keep it as small as possible (not included are: any opencl/spir/spir-v run-time files, llvm test/unittests, clang tests/unittests)    * NOTE: SPIRV-LLVM and SPIR have been ported from 3.6.1 to 3.8.0, but there still might be issues with this    * NOTE: proper Makefile support has been added to SPIRV-LLVM and SPIR tools/lib, as building with cmake still has issues (i.e. can still build with configure/make for now)    * NOTE: llvm and clang patches are now in separate files, because I'm basing this off the llvm and clang git mirrors now, which are two separate repositories (this should make future updates somewhat easier)

---
+++ etc/llvm38/pkg.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+RELEASE=3.8.0
+RELEASE_VERSION_NAME=380
+
+VERSION=$(date +%Y%m%d_%H%M)
+TOOLCHAIN_NAME="compute_toolchain_${RELEASE_VERSION_NAME}_${VERSION}"
+mkdir ${TOOLCHAIN_NAME}
+cd ${TOOLCHAIN_NAME}
+
+mkdir bin
+cp ../build/Release/bin/clang bin/
+cp ../build/Release/bin/llc bin/
+cp ../build/Release/bin/llvm-dis bin/
+cp ../build/Release/bin/llvm-as bin/
+# TODO
+#cp ../build/Release/bin/spir-encoder bin/
+#cp ../build/Release/bin/spir-verifier bin/
+#cp ../build/Release/bin/applecl-encoder bin/
+cp ../build/Release/bin/llvm-spirv bin/
+# TODO
+#cp ../build/Release/bin/spirv-as bin/
+#cp ../build/Release/bin/spirv-dis bin/
+#cp ../build/Release/bin/spirv-val bin/
+
+cp -R ../build/Release/lib/clang/${RELEASE}/include clang
+
+cp -R ../libcxx/include libcxx
+cp ../libcxx/{LICENSE.TXT,CREDITS.TXT} libcxx/
+
+BUILD_PLATFORM=$(uname | tr [:upper:] [:lower:])
+case ${BUILD_PLATFORM} in
+	"mingw"*)
+		# also copy all necessary .dll files
+		cp "/mingw64/bin/libgcc_s_seh-1.dll" bin/
+		cp "/mingw64/bin/libstdc++-6.dll" bin/
+		cp "/mingw64/bin/libwinpthread-1.dll" bin/
+		cp "/mingw64/bin/zlib1.dll" bin/
+		;;
+	*)
+		;;
+esac
+
+cd ..
+
+
+zip -qr ${TOOLCHAIN_NAME}.zip ${TOOLCHAIN_NAME}
+zip_ret_code=$?
+if [ ${zip_ret_code} -ne 0 ]; then
+	echo "failed to create toolchain archive!"
+	exit 1
+else
+	echo "sucessfully created toolchain archive: ${TOOLCHAIN_NAME}.zip"
+fi
