243a527e648ea27c2e749a7f5b9b7a8631eb04d0
Florian Ziesche <florian.ziesche@gmail.com>
2020-12-27 19:30:12 +0100

    * clang/llvm 8.0 toolchain update: rebased SPIRV-Tools on top of KhronosGroup/SPIRV-Tools 17ffa89097b26efeb323e6963220326b5ffb2baf

--- etc/llvm80/build.sh
+++ etc/llvm80/build.sh
@@ -116,7 +116,7 @@ rm -Rf SPIRV-Tools 2>/dev/null
 git clone git://github.com/a2flo/SPIRV-Tools.git
 git clone git://github.com/KhronosGroup/SPIRV-Headers.git SPIRV-Tools/external/spirv-headers
 cd SPIRV-Tools/external/spirv-headers
-git reset --hard 4b0985f29277a81f5ce88feb0502cc44d6d5e7c3
+git reset --hard f027d53ded7e230e008d37c8b47ede7cd308e19d
 cd ../../../
 
 # patch
