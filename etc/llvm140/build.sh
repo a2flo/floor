e8fd990ed6ee60fba750b66dbf39f83a9e283757
Florian Ziesche <florian.ziesche@gmail.com>
2023-05-19 19:11:49 +0200

    * 14.0 toolchain update: Vulkan updates/improvements:    * updated/ported dxil-spirv CFG structurizer to the latest version (now at f20a0fb4e984a83743baa9d863eb7b26228bcca3)    * reintroduced the ability to write debug information:      * added DebugInfoEIS::SPIRV_Debug_Vulkan to signal that we want Vulkan compatible debug info (i.e. SPIR-V w/o any debug extension)      * made most functions in LLVMToSPIRVDbgTran virtual      * added new LLVMToSPIRVDbgTranVulkan that overrides most of LLVMToSPIRVDbgTran to only write OpLine info and nothing else      * SPIRVWriter: when Vulkan/GLSL debug info is requested, this will now create a LLVMToSPIRVDbgTranVulkan instead of a LLVMToSPIRVDbgTran      * SPIRVContainerWriterPass: request Vulkan/GLSL compatible debug info

--- etc/llvm140/build.sh
+++ etc/llvm140/build.sh
@@ -51,8 +51,8 @@ done
 rm -Rf build 2>/dev/null
 
 # download src
-LLVM_REV=56c315961b882315b5927c06c698fb0903217652
-LLVM_SPIRV_REV=8ffd68abb955e3349e85aef82d92c36005d1b364
+LLVM_REV=57a3414d72f81cb5cab139a575b90914af697ad0
+LLVM_SPIRV_REV=e4d9d23015ba20d93c9a7c5552bc7814c6c4dd5d
 if [ ! -d llvm ]; then
 	git clone -b floor_toolchain_1406 https://github.com/a2flo/floor_llvm.git llvm
 	cd llvm
