61af8327860d5cfb1a2668959bf1e931b12da765
Florian Ziesche <florian.ziesche@gmail.com>
2023-08-19 18:50:38 +0200

    * 14.0 toolchain update: fixed building with C++17

--- etc/llvm140/build.sh
+++ etc/llvm140/build.sh
@@ -52,7 +52,7 @@ rm -Rf build 2>/dev/null
 
 # download src
 LLVM_REV=0fe666f476cdb3b357a2e665defaf0b411b9d08e
-LLVM_SPIRV_REV=38edbd98c5aed2e453263eed698ea009605fee43
+LLVM_SPIRV_REV=3902bcecc5311fe5be6942304487e0c440d9b1d5
 if [ ! -d llvm ]; then
 	git clone -b floor_toolchain_1406 https://github.com/a2flo/floor_llvm.git llvm
 	cd llvm
