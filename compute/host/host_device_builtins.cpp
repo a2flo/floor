2f91f5b55ed26ee936b35e1060e965bfb851717c
Florian Ziesche <florian.ziesche@gmail.com>
2024-01-18 22:28:18 +0100

    * host_device_builtins: wrap more external functions that we may need with host-compute

--- compute/host/host_device_builtins.cpp
+++ compute/host/host_device_builtins.cpp
@@ -43,8 +43,56 @@ FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(sinf)(const float va
 FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(cosf)(const float val) FLOOR_HOST_COMPUTE_CC {
 	return ::cosf(val);
 }
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(tanf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::tanf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(asinf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::asinf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(acosf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::acosf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(atanf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::atanf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(atan2f)(const float a, const float b) FLOOR_HOST_COMPUTE_CC {
+	return ::atan2f(a, b);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(sinhf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::sinhf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(coshf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::coshf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(tanhf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::tanhf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(asinhf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::asinhf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(acoshf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::acoshf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(atanhf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::atanhf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(expf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::expf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(exp2f)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::exp2f(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(logf)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::logf(val);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(log2f)(const float val) FLOOR_HOST_COMPUTE_CC {
+	return ::log2f(val);
+}
 FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(powf)(const float a, const float b) FLOOR_HOST_COMPUTE_CC {
 	return ::powf(a, b);
+}
+FLOOR_HOST_DEVICE_FUNC_DECL float FLOOR_HOST_DEVICE_BUILTIN(fmodf)(const float a, const float b) FLOOR_HOST_COMPUTE_CC {
+	return ::fmodf(a, b);
 }
 
 FLOOR_POP_WARNINGS()
@@ -55,7 +103,23 @@ const std::unordered_map<std::string, std::string>& floor_get_c_to_floor_builtin
 	static const std::unordered_map<std::string, std::string> func_map {
 		FLOOR_FUNC_ENTRY(sinf)
 		FLOOR_FUNC_ENTRY(cosf)
+		FLOOR_FUNC_ENTRY(tanf)
+		FLOOR_FUNC_ENTRY(asinf)
+		FLOOR_FUNC_ENTRY(acosf)
+		FLOOR_FUNC_ENTRY(atanf)
+		FLOOR_FUNC_ENTRY(atan2f)
+		FLOOR_FUNC_ENTRY(sinhf)
+		FLOOR_FUNC_ENTRY(coshf)
+		FLOOR_FUNC_ENTRY(tanhf)
+		FLOOR_FUNC_ENTRY(asinhf)
+		FLOOR_FUNC_ENTRY(acoshf)
+		FLOOR_FUNC_ENTRY(atanhf)
+		FLOOR_FUNC_ENTRY(expf)
+		FLOOR_FUNC_ENTRY(exp2f)
+		FLOOR_FUNC_ENTRY(logf)
+		FLOOR_FUNC_ENTRY(log2f)
 		FLOOR_FUNC_ENTRY(powf)
+		FLOOR_FUNC_ENTRY(fmodf)
 	};
 #undef FLOOR_FUNC_ENTRY
 	return func_map;
