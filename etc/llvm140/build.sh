a5dd9d8254ff60845295fbc2eb2925500c8e856f
Florian Ziesche <florian.ziesche@gmail.com>
2024-06-16 23:13:29 +0200

    * 14.0 toolchain update: updated Metal IR workarounds:    * MetalFinal: revisit the air.convert.* handling: since at least Metal 3.0, it is no longer necessary to split vectors into scalars to perform the conversion -> no longer perform the splitting and make call_conversion_func() work with both scalar and vector (1D/2D/3D/4D) types    * MetalFinal: fully remove handling/splitting of vector types for non fp/int <-> fp/int conversions    * MetalFinal: removed obsolete load/store op splitting (this had been disabled for a while and is no longer of any use -> just remove it)

--- etc/llvm140/build.sh
+++ etc/llvm140/build.sh
@@ -54,7 +54,7 @@ done
 rm -Rf build 2>/dev/null
 
 # download src
-LLVM_REV=8d1abead91017c08683f720cdb0818ed72ab1a45
+LLVM_REV=6157fc72d8e4ec80235d04def8d5828a87fd64ae
 LLVM_SPIRV_REV=0c1467251aa87ccdf4f25098101ea5fd480e97a5
 if [ ! -d llvm ]; then
 	git clone -b floor_toolchain_1406 https://github.com/a2flo/floor_llvm.git llvm
