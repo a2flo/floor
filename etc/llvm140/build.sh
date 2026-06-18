54b0113a2907e9b14eb43a95658d6c413b4cade4
Florian Ziesche <florian.ziesche@gmail.com>
2026-06-18 09:26:49 +0200

    * 14.0 toolchain update: front-facing flag support + fixes:    * Metal/Vulkan: added support for primitive "front-facing" flag in fragment shaders    * CGCall: on the Vulkan side, use type coercion to properly handle bool* pointer type translation for input variables (would usually get translated as i8*, but we need i1*)    * CGCall: bitcast I/O types to their (hopefully) non-IO type variant when there is a type mismatch when lowering normal function calls

--- etc/llvm140/build.sh
+++ etc/llvm140/build.sh
@@ -54,7 +54,7 @@ done
 rm -Rf build 2>/dev/null
 
 # download src
-LLVM_REV=adb7ad1423b3e28c8ba9910d57bbe69664b24863
+LLVM_REV=663788a3744a752a983cfd9903d38f24e263dca2
 LLVM_SPIRV_REV=17d414557f84e8bee4f22a33f54c725dc608d970
 if [ ! -d llvm ]; then
 	git clone -b floor_toolchain_1406 https://github.com/a2flo/floor_llvm.git llvm
