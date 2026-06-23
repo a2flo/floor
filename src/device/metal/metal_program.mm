b11afba5040d7ca60af7870d699edc3cfaee5efd
Florian Ziesche <florian.ziesche@gmail.com>
2026-06-23 15:02:19 +0200

    * metal_program: update enums after Xcode 27b2 changes

--- src/device/metal/metal_program.mm
+++ src/device/metal/metal_program.mm
@@ -309,12 +309,12 @@ static const char* metal_tensor_data_type_to_string(const MTLTensorDataType& dat
 		case MTLTensorDataTypeUInt4: return "TensorUInt4";
 #endif
 #if defined(__MAC_27_0) || defined(__IPHONE_27_0) || defined(__VISIONOS_27_0)
-		case MTLTensorDataTypeFloat8UE8M0: return "TensorFloat8UE8M0";
+		case MTLTensorDataTypeMetalFloat8UE8M0: return "TensorFloat8UE8M0";
 		case MTLTensorDataTypeUInt2: return "TensorUInt2";
 		case MTLTensorDataTypeInt2: return "TensorInt2";
-		case MTLTensorDataTypeFloat8E5M2: return "TensorFloat8E5M2";
-		case MTLTensorDataTypeFloat8E4M3: return "TensorFloat8E4M3";
-		case MTLTensorDataTypeFloat4E2M1: return "TensorFloat4E2M1";
+		case MTLTensorDataTypeMetalFloat8E5M2: return "TensorFloat8E5M2";
+		case MTLTensorDataTypeMetalFloat8E4M3: return "TensorFloat8E4M3";
+		case MTLTensorDataTypeMetalFloat4E2M1: return "TensorFloat4E2M1";
 #endif
 	}
 	return "<unknown>";
